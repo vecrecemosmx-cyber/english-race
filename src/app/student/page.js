44bbc621e99095f6f5c40ecf24255c2589d3a877
vecrecemosmx-cyber <vecrecemosmx@gmail.com>
2026-09-04 13:25:00 -0600

    Update page.js

--- src/app/student/page.js
+++ src/app/student/page.js
@@ -790,6 +790,7 @@ function PlataformaFonica() {
                     backgroundColor: '#F4F7FA',
                     color: '#475569',
                     fontSize: '13px', // Redúcelo aquí a '9px' o auméntalo a '11px' según necesites
+                    borderRadius: '12px',
                   }}
                   className="audio-btn hover:opacity-85 !font-black text-#1E293B tracking-tight flex items-center justify-center gap-1 !py-2.5 !px-2 !w-full h-10 shadow-sm rounded-xl transition-all duration-200"
                   title="Volver a escuchar las instrucciones de bienvenida"
