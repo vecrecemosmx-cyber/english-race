59cacf36d53d33f8c9bc596633e425e5d487d9ec
vecrecemosmx-cyber <vecrecemosmx@gmail.com>
2026-09-11 19:07:41 -0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -30,6 +30,7 @@ function LoginForm() {
   const whitelist = [
     "vecrecemosmx@gmail.com",
     "gael.lpzes.9@gmail.com",
+    "luisa.gael.9@gmail.com",
     "virydlr14@gmail.com",
     "bixa.cp@gmail.com",
     "aguilardefuego@gmail.com",
