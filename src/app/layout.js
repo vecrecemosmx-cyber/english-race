08aabf421fba5c27308be25207ddfeaaf1d0ea99
vecrecemosmx-cyber <vecrecemosmx@gmail.com>
2026-09-19 13:17:54 -0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,19 +24,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="es">
-      <head>
-        {/* Inyección de la tipografía Comfortaa desde Google Fonts de forma gratuita */}
-        <link rel="preconnect" href="https://googleapis.com" />
-        <link rel="preconnect" href="https://gstatic.com" crossOrigin="true" />
-        <link href="https://googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="antialiased bg-slate-900 text-slate-100">
-        {children}
-      </body>
-    </html>
-  );
-}
